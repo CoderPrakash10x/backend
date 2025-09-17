8ae77f9828cba051aaaf1c13c675c7880906c51e
CoderPrakash10x <mr.prakash10x@gmail.com>
2025-09-17 23:50:05 +0530

    correct

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
-app.use("/api/contact", contactRoutes);
+app.use("/api", contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
